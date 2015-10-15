5281181590e221715c22b36cd3f812106106671b
Arushi Aneja <arushianeja21@gmail.com>
2015-10-15 20:27:39 +0100

    probability calculation

--- smsspamcollection/filtering.py
+++ smsspamcollection/filtering.py
@@ -3,7 +3,7 @@ def populate_third_dict(good, bad, word, ngood, nbad):
     b = bad[word] or 0
 
     if (g+b >= 5):
-        returnVal = max(0.01, (min 0.99, min(1, b/nbad)/(min(1, (g/ngood)+ min(1, (b/nbad)))))
+        returnVal = max(0.01, (min (0.99, min(1, b/nbad)/(min(1, (g/ngood)+ min(1, (b/nbad)))))
         return returnVal;
     else:
         return 0.4
